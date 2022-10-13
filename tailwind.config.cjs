d9026aee4345d0be983100329d2991651de0d78a
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-10-13 20:18:13 -0300

    Updated tailwind config with tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,34 @@ module.exports = {
     "./src/**/*.tsx"
   ],
   theme: {
-    extend: {},
+    colors: {
+      black: "#000",
+      white: "#fff",
+      transparent: "transparent",
+      gray: {
+        900: "#121214",
+        800: "#202024",
+        400: "#7c7c8a",
+        100: "#e1e1e6",
+      },
+      cyan: {
+        500: "#81b8f7",
+        300: "#b0eaff"
+      }
+    },
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32
+    },
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif"
+      }
+    },
   },
   plugins: [],
 }
